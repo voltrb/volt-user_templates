34fc03044ac6c255e8495c4f50e6ec13446dbb0b
Denny Mueller <denny@dennymueller.de>
2015-02-10 20:13:01 +0100

    use current_user instead of user volt/volt#100

--- app/user-templates/controllers/menu_controller.rb
+++ app/user-templates/controllers/menu_controller.rb
@@ -1,8 +1,8 @@
 module UserTemplates
   class MenuController < Volt::ModelController
     def show_name
-      user = Volt.user
-      user._name.or(user._email).or(Volt.user._username)
+      user = Volt.current_user
+      user._name.or(user._email).or(user._username)
     end
   end
 end
